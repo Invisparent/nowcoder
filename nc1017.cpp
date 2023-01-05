c8bfae5fb82796c23ef4bb1884f963c34e713d6e
Invisparent <101492432+Invisparent@users.noreply.github.com>
2023-01-05 09:27:30 +0800

    Create nc1017.cpp

---
+++ nc1017.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <utility>
+#include <algorithm>
+
+std::vector<std::pair<int, std::string>> globalDat;
+
+inline std::string enquiry(int x, int k) {
+    int t = 0;
+    for (const std::pair<int, std::string> &p: globalDat) {
+        if (p.first % 10000 == x)
+            ++t;
+        if (t == k)
+            return p.second;
+    }
+    return "114514";
+}
+
+int main() {
+    int n, m;
+    std::cin >> n >> m;
+    std::pair<int, std::string> p;
+    for (int t = 0; t < n; ++t) {
+        std::cin >> p.second >> p.first;
+        globalDat.push_back(p);
+    }
+    std::sort(globalDat.begin(), globalDat.end(),
+              [](const std::pair<int, std::string> &a, const std::pair<int, std::string> &b) {
+                  return a.first < b.first;
+              });
+    while (std::cin >> m >> n)
+        std::cout << enquiry(n, m) << std::endl;
+    return 0;
+}
