640368905bbcf2a45643dce8f44b2b2496b24823
Invisparent <invisparent@outlook.com>
2023-01-22 11:11:56 +0800

    Create nc1025-DNF.c

---
+++ 24213/nc1025-DNF.c
@@ -0,0 +1,41 @@
+// https://ac.nowcoder.com/acm/contest/24213/1025
+// 队伍配置
+
+// DNF
+
+//#include <stdio.h>
+//#include <string.h>
+//
+//#define maxn 305
+//#define maxm 305
+//#define maxd 145
+//#define maxdp 6
+//
+//int dp[maxdp][maxd];
+//
+//typedef struct x {
+//    int atk, cost;
+//} x;
+//
+//x mdat[maxn], fdat[maxm];
+//
+//int max(int a, int b) {
+//    if (a > b)
+//        return a;
+//    return b;
+//}
+//
+//int main() {
+//    int n, m, d;
+//    scanf("%d%d%d", &n, &m, &d);
+//    for (int t = 1; t <= n; ++t)
+//        scanf("%d%d", &mdat[t].atk, &mdat[t].cost);
+//    for (int t = 1; t <= m; ++t)
+//        scanf("%d%d", &fdat[t].atk, &fdat[t].cost);
+//    int dplim = max(5, n);
+//    for (int i = 1; i <= dplim; ++i) {
+//        for (int j = i; j <= n; ++j) {
+//
+//        }
+//    }
+//}
