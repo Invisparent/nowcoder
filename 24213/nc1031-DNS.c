fbc5f591b34408cf6a387dd685aebc0a43accb37
Invisparent <invisparent@outlook.com>
2023-01-23 11:38:12 +0800

    Update

--- 24213/nc1031-DNS.c
+++ 24213/nc1031-DNS.c
@@ -1,4 +1,4 @@
-// https://ac.nowcoder.com/acm/contest/24213/1030
+// https://ac.nowcoder.com/acm/contest/24213/1031
 // 石子合并
 
 // DNS
---
+++ 24213/nc1032.c
@@ -0,0 +1,2 @@
+// https://ac.nowcoder.com/acm/contest/24213/1032
+// 
